e717cdc9439ce5b5e34776f3f2ecc7a72f53b72d
Dbowen89 <Dbowen89>
2022-01-27 23:16:05 -0500

    Modified datarequired messages in forms

--- app/forms.py
+++ app/forms.py
@@ -3,8 +3,8 @@ from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Email
 
 class ContactForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired(message="Name is required")])
-    email = StringField("Email", validators=[DataRequired(message="Email is required"), Email(message="Please enter a valid email address.", check_deliverability=True)])
-    subject = StringField("Subject", validators=[DataRequired(message="Subject is required")])
-    message = TextAreaField("Message", validators=[DataRequired(message="Message is required")])
+    name = StringField("Name", validators=[DataRequired(message="Please enter your name.")])
+    email = StringField("Email", validators=[DataRequired(message="A valid email address is required."), Email(message="Please enter a valid email address.", check_deliverability=True)])
+    subject = StringField("Subject", validators=[DataRequired(message="A subject line is required.")])
+    message = TextAreaField("Message", validators=[DataRequired(message="Please enter a message.")])
     submit = SubmitField("Send")
